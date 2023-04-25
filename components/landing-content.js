87a6ec8e8bb7e73ea389bb0b80c98022368401b8
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2023-04-25 15:42:34 -0600

    font styling

--- components/landing-content.js
+++ components/landing-content.js
@@ -3,6 +3,7 @@ import localFont from "next/font/local"
 
 // Custom
 import Button from "./button"
+import utilStyles from "../styles/utils.module.css"
 
 // Styles
 import styles from "./styles/landing-content-styles.module.scss"
@@ -14,7 +15,7 @@ const LandingContent = () => {
      return (
             <div className={`${styles.textContainer} text-animate`} >
                   <p className={`${myFont.className} ${styles.CTAtext}`} > Hi, I'm Amelia. </p>
-                  <div className={`${styles.CTAsubtext} montserrat`}>
+                  <div className={`${styles.CTAsubtext} ${utilStyles.montserrat}`}>
                         <p style={{margin: "0px"}}> I'm a passionate Front End Developer, UI/UX Designer, and content creator.</p>
                         <p style={{margin: "0px"}}> Let's build something together. </p>
                   </div>
